e81420ad31aede0e948e781e9719410c1b43ca52
unknown <ivanlorenzoruiz@alumnos.tuniversformacion.es>
2024-01-17 09:35:59 +0100

    Creada la lógica básica de javascript

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,22 @@
 @autor: Iván Lorenzo Ruiz
 github:
 */
+
+const formulario=document.querySelector("#formulario")
+const nombre=document.querySelector("#nombre")
+const correo=document.querySelector("#correo")
+const solicitud=document.querySelector("#solicitud")
+const reserva=document.querySelector("#reserva")
+const info=document.querySelector("#info")
+const enviar=document.querySelector("#enviar")
+const errores=document.querySelector("#errores")
+let erroresMensaje= []
+
+function validar (evento) {
+    evento.preventDefault()
+    erroresMensaje=[]
+
+    nombre.value.trim().length===0 && alert("EL nombre no puede estar vacio")
+}
+
+formulario.addEventListener("submit", validar)
